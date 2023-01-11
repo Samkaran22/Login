f59430003d0264a6377ad0d557fa762df645a88b
Samkaran22 <karankumarsk2209@gmail.com>
2023-01-11 08:41:51 +0530

    Add existing file

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -7,37 +7,6 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  constructor(private fireauth : AngularFireAuth,private router : Router) { }
-
-  //login
-  login(email : string,password: string){
-    this.fireauth.signInWithEmailAndPassword(email,password).then( () =>{
-      localStorage.setItem('token','true');
-    }, err => {
-       alert(err.message)
-       this.router.navigate(['/']);
-    })
-  }
-   //singnup
-   signup(email : string,password : string){
-    this.fireauth.createUserWithEmailAndPassword(email,password).then( () => {
-    alert('Registration Successful')
-    this.router.navigate(['/']);
-    }, err =>{
-       alert(err.message);
-       this.router.navigate(['/signup'])
-    })
-   }  
-
-   //logout
-   logout(){
-    this.fireauth.signOut().then( () =>{
-     localStorage.removeItem('token');
-     this.router.navigate(['/']);
-    }, err =>{
-       alert(err.message);
-    })
-   }
 }
 
 
